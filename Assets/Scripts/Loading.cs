af2afbde7cf9ce8539e65f5be4c9474a86003312
Fauzi <rizael.ichigo28@gmail.com>
2019-12-05 10:25:34 +0700

    create loading and setting scene

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] private float delayLoading = 3f;
+    [SerializeField] private string nameScene;
+    private float timeElapsed;
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > delayLoading) {
+            SceneManager.LoadScene(nameScene);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMenu : MonoBehaviour
+{
+    public void MeneScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void ExitScene()
+    {
+        Application.Quit();
+    }
+     
+}
